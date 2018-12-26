28c920627d71cbcf095a98287163d9aae18b886e
Dmitry Gerasimov <di.gerasimov@gmail.com>
2018-12-27 00:45:50 +0300

    Working IMA appraisal

--- README.md
+++ README.md
@@ -2,18 +2,46 @@
 
 ## Required packages
 
-    build-essential bc cpio flex libncurses5-dev qemu-system-x86
+    build-essential bc flex libncurses5-dev ima-evm-utils openssl qemu-system-x86
 
 ## Build
 
     ./build.sh
+
+There are certificate and key will be generated on the first build stage,
+so you will be asked to type in some password.
+
+Note: for somewhat reason, kernel does not accepts our dropped-in cert
+on first build. To work around this, just run `./build.sh` twice.
 
 ## Run
 
     qemu-system-x86_64 \
         --nographic \
         --kernel out/bzImage \
-        --initrd out/initramfs.cpio.gz \
-        --append "console=ttyS0"
+        --drive file=out/system.img,if=virtio \
+        --append "console=ttyS0 ima_appraise=enforce root=/dev/vda init=/init"
 
 Any time you want to terminate QEMU, just press `Ctrl + A, X`.
+
+As busybox is signed, IMA will not complain about it. You can try
+to run some unsigned executable to see how it works, e.g:
+
+    hello-unsigned
+
+You may also run kernel with `ima_appraise=log`. This will allow you to run
+any binaries (either signed or unsigned), and see messages in audit log
+if binary was not signed.
+
+## Required kernel configuration
+
+All required kernel configuration options are already included in `config/linux-XXX`.
+But if you want to configure kernel by yourself, take a look at following options:
+
+    TODO
+
+## More info
+
+- [IMA/EVM (openSUSE support database)](https://en.opensuse.org/SDB:Ima_evm)
+- [evmctl - IMA/EVM signing utility](http://linux-ima.sourceforge.net/evmctl.1.html)
+- [Configuring Linux Kernel with QEMU guest support](https://wiki.gentoo.org/wiki/QEMU/Linux_guest)
--- build.sh
+++ build.sh
@@ -7,6 +7,16 @@ BUSYBOX_TAR=$BUSYBOX.tar.bz2
 
 BUILD_ROOT=$(pwd)
 
+build_keys() {
+    cd $BUILD_ROOT/keys
+    if [ ! -f ima-local-ca.pem ]; then
+        ./ima-gen-local-ca.sh
+    fi
+    if [ ! -f x509_ima.der ]; then
+        ./ima-genkey.sh
+    fi
+}
+
 build_kernel() {
     cd $BUILD_ROOT
 
@@ -18,6 +28,7 @@ build_kernel() {
     fi
 
     cp config/$KERNEL-config $KERNEL/.config
+    cp keys/ima-local-ca.pem $KERNEL/certs/signing_key.pem
     cd $KERNEL
     make oldconfig
     make -j8
@@ -43,23 +54,37 @@ build_busybox() {
     make install
 }
 
-build_initramfs() {
-    rm -rf $BUILD_ROOT/initramfs
-    mkdir -vp $BUILD_ROOT/initramfs
+build_ext4() {
+    cd $BUILD_ROOT
+
+    dd if=/dev/zero of=out/system.img bs=1M count=100
+    sudo mkfs.ext4 out/system.img
+    mkdir -p system
+    sudo mount out/system.img system
 
     cd $BUILD_ROOT/$KERNEL
-    make modules_install INSTALL_MOD_PATH=$BUILD_ROOT/initramfs
+    sudo make modules_install INSTALL_MOD_PATH=$BUILD_ROOT/system
 
-    cd $BUILD_ROOT/initramfs
-    mkdir -vp bin sbin etc proc sys usr/bin usr/sbin
-    cp -a $BUILD_ROOT/$BUSYBOX/_install/* .
-    cp -a $BUILD_ROOT/prebuilt/* .
+    cd $BUILD_ROOT/system
+    sudo mkdir -vp dev bin sbin etc/keys proc sys tmp usr/bin usr/sbin
+    sudo cp -a $BUILD_ROOT/$BUSYBOX/_install/* .
+    sudo cp -a $BUILD_ROOT/prebuilt/* .
 
-    find . -print0 | cpio --null -ov --format=newc | gzip -9 \
-        > $BUILD_ROOT/out/initramfs.cpio.gz
+    cd $BUILD_ROOT
+
+    # sign executables
+    sudo evmctl ima_sign -a sha256 --key keys/privkey_ima.pem system/init
+    sudo evmctl ima_sign -a sha256 --key keys/privkey_ima.pem system/bin/busybox
+
+    # built x509 public key will be loaded into kernel keyring on boot
+    sudo cp keys/x509_ima.der system/etc/keys/x509_ima.der
+
+    sudo umount system
+    rm -rf system
 }
 
 mkdir -p out
+build_keys
 build_kernel
 build_busybox
-build_initramfs
+build_ext4
